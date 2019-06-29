d88f4b5f7044d1c777034e0a59c347f5b7b9bc92
mon <mon.kappa@gmail.com>
2019-06-29 18:43:43 +0900

    created

---
+++ CheckBox/CheckBox/CheckBox.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CheckBox
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CheckBox : ContentView
+    {
+        #region BindableProperty
+        public static readonly BindableProperty IsCheckedProperty = BindableProperty.Create(propertyName: nameof(IsChecked), returnType: typeof(bool), declaringType: typeof(CheckBox), defaultValue:false,
+            propertyChanged: (bindable, oldValue, newValue) => { ((CheckBox)bindable).OnChecked((bool)newValue); });
+
+        private void OnChecked(bool newValue)
+        {
+            this.checkOn.IsVisible = IsChecked;
+            this.checkOff.IsVisible = !IsChecked;
+        }
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+        #endregion
+
+        public CheckBox()
+        {
+            InitializeComponent();
+
+            this.checkOn.IsVisible = IsChecked;
+            this.checkOff.IsVisible = !IsChecked;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            IsChecked = !IsChecked;
+        }
+    }
+}
---
+++ CheckBox/CheckBox/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CheckBox
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected bool _isChecked;
+        public bool IsChecked
+        {
+            get { return _isChecked; }
+            set
+            {
+                if (_isChecked == value)
+                    return;
+
+                _isChecked = value;
+                OnPropertyChanged(nameof(IsChecked));
+            }
+        }
+    }
+}
